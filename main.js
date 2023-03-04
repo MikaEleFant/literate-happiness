5cf8d840c12c3b8765d70385dd088d86993fb4dd
MikaEleFant <changholee2wpes@gmail.com>
2023-03-03 16:56:08 -0800

    Declared new Die objects and stored into const variables

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import Die from 'die.js';
 import Spell from 'spell.js';
+import Entity from 'entity.js';
 
-new Die("D4", 4);
-new Die("D6", 6);
-new Die("D8", 8);
-new Die("D10", 10);
-new Die("D12", 12);
-new Die("D20", 20);
+const D4 = new Die("D4", 4);
+const D6 = new Die("D6", 6);
+const D8 = new Die("D8", 8);
+const D10 = new Die("D10", 10);
+const D12 = new Die("D12", 12);
+const D20 = new Die("D20", 20);
