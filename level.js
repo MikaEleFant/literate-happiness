092dcd23465b845d64b3dd46dece0e5faeddfc7f
MikaEleFant <changholee2wpes@gmail.com>
2023-03-07 17:52:24 -0800

    Change Level design

--- level.js
+++ level.js
@@ -1,9 +1,16 @@
 export default class Level {
-    constructor(levelNumber) {
-        this.levelNumber = levelNumber;
+    constructor(difficulty) {
+        this.difficulty = difficulty;
 
-        this.size = [(levelNumber * 5) + 5, (levelNumber * 5) + 5];
-        this.numTraps = levelNumber * 3;
-        this.numEnemies = levelNumber * 2 + 4;
+        if (difficulty === "easy") {
+            this.numRounds = 10;
+            this.timer = 90000;
+        } else if (difficulty === "medium") {
+            this.numRounds = 20;
+            this.timer = 60000;
+        } else {
+            this.numRounds = 30;
+            this.timer = 30000;
+        }
     }
 }
